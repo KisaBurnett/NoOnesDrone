910bfd09dccd68c67ce4b786b3d1f2d6f3b20454
Kisa Burnett <54787854+KisaBurnett@users.noreply.github.com>
2020-04-24 14:05:26 -0500

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Based on code by user Johaneto on answers.unity.com
+    // https://answers.unity.com/users/575168/johaneto.html
+    // https://answers.unity.com/questions/548794/how-to-move-a-camera-only-using-the-arrow-keys.html
+
+    public float rotateSpeed = 100f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalDir = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontalDir, 0);
+    }
+}
