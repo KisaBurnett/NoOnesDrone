0581725904283fa8a35fce1d8b9d5c64d72cd2cc
Kisa Burnett <54787854+KisaBurnett@users.noreply.github.com>
2020-04-30 19:22:37 -0500

    Set up credits

---
+++ Assets/Scripts/ForwardCredits.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForwardCredits : MonoBehaviour
+{
+    public GameObject thisPage;
+    public GameObject nextPage;
+
+    public void nextCredits()
+    {
+        nextPage.gameObject.SetActive(true);
+        thisPage.gameObject.SetActive(false);
+    }
+}
